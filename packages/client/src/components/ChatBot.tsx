0d4c8f49bf215b7191be5927153c5b22a38dc06d
Husam Ahmed <des-techno@hotmail.com>
2026-06-21 00:00:22 +0300

    improved look and feel

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -29,16 +29,16 @@ const ChatBot = () => {
   const { register, handleSubmit, reset, formState } = useForm<FormData>();
   const [messages, setMessages] = useState<Message[]>([]);
   const [isBotTyping, setIsBotTyping] = useState<boolean>(false);
-  const formRef = useRef<HTMLFormElement | null>(null);
+  const lastMessageRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    formRef?.current?.scrollIntoView({ behavior: 'smooth' });
+    lastMessageRef?.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages]);
 
   const onSubmit = async ({ prompt }: FormData) => {
     setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
 
-    reset();
+    reset({ prompt: '' });
 
     setIsBotTyping(true);
 
@@ -68,11 +68,12 @@ const ChatBot = () => {
   };
 
   return (
-    <div>
-      <div className="flex flex-col gap-2 mb-8">
+    <div className="flex flex-col h-full">
+      <div className="flex flex-col flex-1 gap-2 mb-8 overflow-y-auto">
         {messages.map((message, index) => (
           <div
             key={index}
+            ref={index === messages.length - 1 ? lastMessageRef : null}
             onCopy={onCopyMessage}
             className={`px-3 py-1 rounded-xl ${
               message.role === 'user'
@@ -94,7 +95,6 @@ const ChatBot = () => {
       <form
         onSubmit={handleSubmit(onSubmit)}
         onKeyDown={onKeyDown}
-        ref={formRef}
         className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
       >
         <textarea
@@ -102,6 +102,7 @@ const ChatBot = () => {
             required: true,
             validate: (data: string) => data.trim().length > 0,
           })}
+          autoFocus
           className="w-full border-0 focus:outline-0 resize-none"
           placeholder="Ask anything..."
           maxLength={1000}
